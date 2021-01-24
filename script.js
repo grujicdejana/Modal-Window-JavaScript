bbb60240239bbb020db3e71c09a5536731daa7f3
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2021-01-24 14:19:38 +0100

    Open and close with classes

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+function openModal(mdl) {
+  mdl.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  openModal(btnsOpenModal[i]);
+}
+
+function closeModal(mdl) {
+  mdl.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+}
+
+closeModal(btnCloseModal);
+closeModal(overlay);
