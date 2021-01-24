8c411ec0ec2bd8ebb5f29404777dccbc9d8dffd1
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2021-01-24 15:28:22 +0100

    Esc Keypress Event

--- script.js
+++ script.js
@@ -5,23 +5,32 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-function openModal(mdl) {
-  mdl.addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  openModal(btnsOpenModal[i]);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 
-function closeModal(mdl) {
-  mdl.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  });
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 }
 
-closeModal(btnCloseModal);
-closeModal(overlay);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keyup', function (e) {
+  //console.log(e);
+
+  // (e.key === 'Escape')
+  if (e.keyCode === 27) {
+    //close the model, when the model is actually visible.
+    //if the model contains the class visible, it means that it's not visible.
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
